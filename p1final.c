1f59b148ecf54a4da974d930ba4db2841b6a1ee1
guna sekhar <93110449+Gunasekhar2002@users.noreply.github.com>
2022-03-05 03:53:43 +0000

    p1

--- p1final.c
+++ p1final.c
@@ -1,1 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("enter num1:");
+  scanf("%d",num1);
+   printf("enter den1:");
+  scanf("%d",den1);
+   printf("enter num2:");
+  scanf("%d",num2);
+   printf("enter den2:");
+  scanf("%d",den2);
+  
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  *num3=num1*den2+num2*den1;
+  *den3=den1*den2;
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  printf("sum of fraction is:\n");
+  printf("%d/%d+%d/%d=%d/%d",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int n1,n2,n3,d1,d2,d3;
+  input(&n1,&d1,&n2,&d2);
+  add(n1,d1,n2,d2,&n3,&d3);
+  output(n1,d1,n2,d2,n3,d3);
+  return 0;
+}
