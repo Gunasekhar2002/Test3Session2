94c1aa4edafc4a789466686bd8d047ccffc05c8a
guna sekhar <93110449+Gunasekhar2002@users.noreply.github.com>
2022-03-08 15:20:37 +0000

    p7

--- p7final.c
+++ p7final.c
@@ -1,21 +1,33 @@
 #include<stdio.h>
 struct _fraction
 {
-  int num,den;
+int num,den;
 };
 typedef struct _fraction Fraction;
-  int gcd(int a,int b)
-  {
-    int t;
-    while (b != 0)
-      {
-        t = b;
-        b = a%b;
-        a = t;
-      }
-    return a;
+
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("enter the numerator and denominator:");
+  scanf("%d %d",&f.num,&f.den);
+  return f;
+}
+Fraction add_fractions(Fraction f1, Fraction f2)
+{
+  int sum;
+  sum=(f1.num/f1.den)+(f2.num/f2.den);
+}
+void output(Fraction f1, Fraction f2,Fraction sum)
+{
+  printf("sum of two fractions=%d",sum);
 }
 int main()
 {
-  printf("%d\n",gcd(16,24));
-}
+  Fraction f1,f2,f3,sum;
+  f1=input_fraction();
+  f2=input_fraction();
+ 
+  sum=add_fractions(f1,f2);
+  output(f1,f2,sum);
+  return 0;
+  }  
